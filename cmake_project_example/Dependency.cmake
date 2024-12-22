6ddd5826969e8356a833781f10b3526285f48c7e
Jeon Haechan <geojeonhc@gmail.com>
2024-12-22 15:19:11 +0900

    BUILD: Dependency.cmake 내 Glfw3 의존성 추가

--- cmake_project_example/Dependency.cmake
+++ cmake_project_example/Dependency.cmake
@@ -23,3 +23,22 @@ ExternalProject_Add(
 # Dependency 리스트 및 라이브러리 파일 리스트 추가
 set(DEP_LIST ${DEP_LIST} dep_spdlog)
 set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+
+# glfw
+ExternalProject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3.2"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLFW_BUILD_EXAMPLES=OFF
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+    TEST_COMMAND ""
+)
+
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
