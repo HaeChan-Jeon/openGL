b9a0e038da8d1f318fb52dfedfcb3677fea28a93
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2024-12-22 17:31:47 +0900

    FEAT: glfwSetKeyCallback 설정
    glfwSetKeyCallback 내 OnKeyEvent 작성 및 인자 설정

--- first_opengl_example/src/main.cpp
+++ first_opengl_example/src/main.cpp
@@ -4,8 +4,24 @@
 
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
     SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
-    
+
     glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key,
+        scancode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
 
 int main(int argc, const char** argv){
@@ -63,6 +79,7 @@ int main(int argc, const char** argv){
     SPDLOG_INFO("OpenGL context version: {}", std::string(reinterpret_cast<const char*>(glVersion)));
 
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
 
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
