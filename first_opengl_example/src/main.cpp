f47ebb381086a69fca591d0544ebec0571373f3f
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2024-12-22 17:17:54 +0900

    FEAT: Glad 초기화 및 glad를 활용한 OpenGL 함수 로딩
    glVersion이 비-void 형식임에 따라, reinterpret_cast 추가 수행

--- first_opengl_example/src/main.cpp
+++ first_opengl_example/src/main.cpp
@@ -1,4 +1,5 @@
 #include <spdlog/spdlog.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv){
@@ -17,6 +18,10 @@ int main(int argc, const char** argv){
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
 
@@ -28,6 +33,8 @@ int main(int argc, const char** argv){
         nullptr
     );
 
+    glfwMakeContextCurrent(window);
+
     if (!window) {
         SPDLOG_ERROR("failed to create glfw window");
 
@@ -35,6 +42,19 @@ int main(int argc, const char** argv){
 
         return -1;
     }
+
+    // glad를 활용한 OpenGL 함수 로딩
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_ERROR("failed to initialize glad");
+    
+        glfwTerminate();
+    
+        return -1;
+    }
+
+    const GLubyte* glVersion = glGetString(GL_VERSION);
+
+    SPDLOG_INFO("OpenGL context version: {}", std::string(reinterpret_cast<const char*>(glVersion)));
 
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
