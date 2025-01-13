3ecc40d260d5e6d75e226b84765e756ffd13323e
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-01-13 21:57:18 +0900

    FEAT: 체커 이미지의 생성 방법 구현현

--- opengl_example/src/image.h
+++ opengl_example/src/image.h
@@ -9,6 +9,8 @@ class Image {
 public:
     static ImageUPtr Load(const std::string& filepath);
 
+    static ImageUPtr Create(int width, int height, int channelCount = 4);
+
     ~Image();
 
     const uint8_t* GetData() const { return m_data; }
@@ -17,10 +19,14 @@ public:
     int GetHeight() const { return m_height; }
     int GetChannelCount() const { return m_channelCount; }
 
+    void SetCheckImage(int gridX, int gridY);
+
 private:
     Image() {};
 
     bool LoadWithStb(const std::string& filepath);
+
+    bool Allocate(int width, int height, int channelCount);
 
     int m_width { 0 };
     int m_height { 0 };
