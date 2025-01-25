c0cdd20bb637d0f4c5c5b1409b41cb3aa6e9f715
JeonHaeChan <geojeonhc@gmail.com>
2025-01-25 21:24:23 +0900

    FEAT: ADD glm Dependency

--- opengl_example/Dependency.cmake
+++ opengl_example/Dependency.cmake
@@ -72,7 +72,24 @@ ExternalProject_Add(
         ${PROJECT_BINARY_DIR}/dep_stb-prefix/src/dep_stb/stb_image.h
         ${DEP_INSTALL_DIR}/include/stb/stb_image.h
 )
+
+# glm
+ExternalProject_Add(
+    dep_glm
+    GIT_REPOSITORY "https://github.com/g-truc/glm"
+    GIT_TAG "0.9.9.8"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    TEST_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory
+    ${PROJECT_BINARY_DIR}/dep_glm-prefix/src/dep_glm/glm
+    ${DEP_INSTALL_DIR}/include/glm
+)
     
 set(DEP_LIST ${DEP_LIST} dep_glad)
 set(DEP_LIBS ${DEP_LIBS} glad)
 set(DEP_LIST ${DEP_LIST} dep_stb)
+set(DEP_LIST ${DEP_LIST} dep_glm)
