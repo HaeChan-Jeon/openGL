175244d38a56625874007a1c5be7955b65e3d390
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-02-06 21:39:20 +0900

    FEAT: 금속 등 Final Lighting Maps

--- opengl_example/src/context.cpp
+++ opengl_example/src/context.cpp
@@ -162,7 +162,8 @@ bool Context::Init() {
     m_texture2 = Texture::CreateFromImage(image2.get());
 
     m_material.diffuse = Texture::CreateFromImage(Image::Load("./image/container2.png").get());
-
+    m_material.specular = Texture::CreateFromImage(Image::Load("./image/container2_specular.png").get());
+    
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, m_texture->Get());
     glActiveTexture(GL_TEXTURE1);
@@ -206,7 +207,6 @@ void Context::Render() {
         
         if (ImGui::CollapsingHeader("material", ImGuiTreeNodeFlags_DefaultOpen))
         {
-            ImGui::ColorEdit3("m.specular", glm::value_ptr(m_material.specular));
             ImGui::DragFloat("m.shininess", &m_material.shininess, 1.0f, 1.0f, 256.0f);
         }
 
@@ -258,11 +258,13 @@ void Context::Render() {
     m_program->SetUniform("light.diffuse", m_light.diffuse);
     m_program->SetUniform("light.specular", m_light.specular);
     m_program->SetUniform("material.diffuse", 0);
-    m_program->SetUniform("material.specular", m_material.specular);
+    m_program->SetUniform("material.specular", 1);
     m_program->SetUniform("material.shininess", m_material.shininess);
 
     glActiveTexture(GL_TEXTURE0);
     m_material.diffuse->Bind();
+    glActiveTexture(GL_TEXTURE1);
+    m_material.specular->Bind();
 
     for (size_t i = 0; i < cubePositions.size(); i++){
         auto& pos = cubePositions[i];
