36bb482da00e63318b3e380f48934c9c27bf48b4
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-02-16 12:00:00 +0900

    FEAT: Framebuffer and Post-Processing

--- opengl_example/src/context.cpp
+++ opengl_example/src/context.cpp
@@ -41,6 +41,7 @@ void Context::Reshape(int width, int height) {
     m_width = width;
     m_height = height;
     glViewport(0, 0, m_width, m_height);
+    m_framebuffer = Framebuffer::Create(Texture::Create(width, height, GL_RGBA));
 }
 
 void Context::MouseMove(double x, double y) {
@@ -94,6 +95,10 @@ bool Context::Init() {
     if (!m_textureProgram)
         return false;
 
+    m_postProgram = Program::Create("./shader/texture.vs", "./shader/gamma.fs");
+    if (!m_postProgram)
+        return false;
+
     glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
 
     TexturePtr darkGrayTexture = Texture::CreateFromImage(
@@ -132,6 +137,7 @@ void Context::Render() {
             glClearColor(m_clearColor.r, m_clearColor.g, m_clearColor.b, m_clearColor.a);
         }
 
+        ImGui::DragFloat("gamma", &m_gamma, 0.01f, 0.0f, 2.0f);
         ImGui::Separator();
         ImGui::DragFloat3("camera pos", glm::value_ptr(m_cameraPos), 0.01f);
         ImGui::DragFloat("camera yaw", &m_cameraYaw, 0.5f);
@@ -158,9 +164,15 @@ void Context::Render() {
         }
 
         ImGui::Checkbox("animation", &m_animation);
+
+        float aspectRatio = (float)m_width / (float)m_width;
+        ImGui::Image((ImTextureID)m_framebuffer->GetColorAttachment()->Get(),
+            ImVec2(150 * aspectRatio, 150));
     }
 
     ImGui::End();
+
+    m_framebuffer->Bind();
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
@@ -269,4 +281,16 @@ void Context::Render() {
     transform = projection * view * modelTransform;
     m_textureProgram->SetUniform("transform", transform);
     m_plane->Draw(m_textureProgram.get());
+
+    Framebuffer::BindToDefault();
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    m_postProgram->Use();
+    m_postProgram->SetUniform("transform",
+        glm::scale(glm::mat4(1.0f), glm::vec3(2.0f, 2.0f, 1.0f)));
+    m_framebuffer->GetColorAttachment()->Bind();
+    m_postProgram->SetUniform("tex", 0);
+    m_postProgram->SetUniform("gamma", m_gamma);
+    m_plane->Draw(m_postProgram.get());
 }
