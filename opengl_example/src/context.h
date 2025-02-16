b9ad5aee75c096dce7065226253e72ac05d3fa5e
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-02-16 14:12:02 +0900

    FEAT: Environment Mapping

--- opengl_example/src/context.h
+++ opengl_example/src/context.h
@@ -73,7 +73,8 @@ private:
     // cubemap
     CubeTextureUPtr m_cubeTexture;
     ProgramUPtr m_skyboxProgram;
-    
+    ProgramUPtr m_envMapProgram;
+
     int m_width {WINDOW_WIDTH};
     int m_height {WINDOW_HEIGHT};
 };
